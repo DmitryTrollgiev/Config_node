971a0823e51ceda003c125858d19fc662607fcfb
Egorus <egrbykov@gmail.com>
2024-04-10 02:17:53 +0300

    node is working

--- index.js
+++ index.js
@@ -1,2 +1,37 @@
-let num = 5
-console.log(`Variable: ${num}`)
+const express = require('express')
+const app = express()
+const PORT = 3000
+
+app.set('view engine', 'ejs')
+app.use(express.static('static'))
+
+
+app.get('/', (req, res) => {
+    res.render('login_page')
+})
+
+app.get('/main', (req, res) => {
+    res.render('main_page')
+})
+
+app.get('/components', (req, res) => {
+    res.render('components_page')
+})
+
+app.get('/configurator', (req, res) => {
+    res.render('configurations_page')
+})
+
+app.get('/reports', (req, res) => {
+    res.render('reports_page')
+})
+app.get('/settings', (req, res) => {
+    res.render('settings_page')
+})
+
+
+
+
+app.listen(PORT, () =>{
+    console.log(`Server started: http://localhost:${PORT}`)
+})
